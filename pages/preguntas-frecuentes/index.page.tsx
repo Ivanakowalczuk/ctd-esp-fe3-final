f2d1e4ae72c433c692648f13d9fca06794c83fdb
ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-03-25 02:36:56 -0300

    faqs front

--- pages/preguntas-frecuentes/index.page.tsx
+++ pages/preguntas-frecuentes/index.page.tsx
@@ -1,35 +1,35 @@
-// import { GetStaticProps, NextPage } from "next";
-// import { FaqsType } from "dh-marvel/components/faqs/faqsData";
-// import BodySingle from 'dh-marvel/components/layouts/body/single/body-single';
-// import Faqs from '../../components/faqs/faqs';
+import { GetStaticProps, NextPage } from "next";
+import { FaqsType } from "dh-marvel/components/faqs/faqsData";
+import BodySingle from 'dh-marvel/components/layouts/body/single/body-single';
+import Faqs from '../../components/faqs/faqs';
 
 
-// interface Props {
-//   faqs: FaqsType[];
-// }
+interface Props {
+  faqs: FaqsType[];
+}
 
-// const Faq: NextPage<Props> = ({ faqs }) => {
-//   return (
+const Faq: NextPage<Props> = ({ faqs }) => {
+  return (
        
-//             <BodySingle title={"Preguntas Frecuentes"}>
-//                 <Faqs faqs={faqs}/>
-//             </BodySingle>
+            <BodySingle title={"Preguntas Frecuentes"}>
+                <Faqs faqs={faqs}/>
+            </BodySingle>
 
   
   
-//   );
-// };
+  );
+};
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const url = "https://marvel-comics-liart.vercel.app";
-//   const res = await fetch(`${url}/api/faqs`);
-//   const faqs = await res.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const url = "https://marvel-comics-liart.vercel.app";
+  const res = await fetch(`${url}/api/faqs`);
+  const faqs = await res.json();
 
-//   return {
-//     props: {
-//       faqs,
-//     },
-//   };
-// };
+  return {
+    props: {
+      faqs,
+    },
+  };
+};
 
-// export default Faq;
+export default Faq;
